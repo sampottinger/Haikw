78b196f70f9d0f322a1fffc8c3964eab7e32a030
A. Samuel Pottinger <Samnsparky@gmail.com>
2011-11-13 13:20:05 -0700

    Added state and object classes - untested

---
+++ object.py
@@ -0,0 +1,288 @@
+"""
+Module containing objects managing simulated objects
+
+This module provides management for objects simulated in the target inverse kinematics package
+
+@author: Sam Pottinger
+@license: GNU General Public License v2
+@copyright: 2011
+@organization: Andrews Robotics Initiative at CU Boulder
+"""
+
+class VirtualObjectColor:
+	"""
+	Simple structure for RBG colors
+	"""
+
+	def __init__(self, r, g, b):
+		""" Construtor for VirtualObjectColor
+
+		@param r: The red component of this color
+		@type r: Byte / integer (0-255)
+		@param g: The green component of this color
+		@type g: Byte / integer (0-255)
+		@param b: The blue component of this color
+		@type b: Byte / integer (0-255)
+		"""
+
+		# Verify ranges
+		if r >= 0 && r <= 255:
+			self.__r = r
+		else:
+			raise ValueError("Invalid value for the provided red value (must be between 0 and 255)")
+
+		if g >= 0 && g <= 255:
+			self.__g = g
+		else:
+			raise ValueError("Invalid value for the provided green value (must be between 0 and 255)")
+
+		if b >= 0 && b <= 255:
+			self.__b = b
+		else:
+			raise ValueError("Invalid value for the provided blue value (must be between 0 and 255)")
+	
+	def get_red():
+		""" Get the red coponent of this color
+
+		@return: The red component of this color
+		@rtype: Byte / integer (range from 0 to 255)
+		"""
+
+		return self.__r
+
+	def get_green():
+		""" Get the green coponent of this color
+
+		@return: The green component of this color
+		@rtype: Byte / integer (range from 0 to 255)
+		"""
+
+		return self.__g
+		
+	def get_blue():
+		""" Get the blue coponent of this color
+
+		@return: The blue component of this color
+		@rtype: Byte / integer (range from 0 to 255)
+		"""
+
+		return self.__b
+
+class VirtualObjectConstructionStrategy:
+	"""
+	Interface / fully abstract parent class for strategies for creating VirtualObjects
+
+	@note: It's a bit un-pythonic to do this but, given long term extendability concerns, this option was taken
+	"""
+
+	def create_object(self, descriptor, color, size):
+		"""
+		Creates a new object with the given properties
+
+		@param descriptor: Simple description of the object to make
+		@type descriptor: String
+		@param color: VirtualObjectColor
+		"""
+
+		raise NotImplementedError("Must use subclass / implementor of this interface")
+
+class ColorResolutionStrategy:
+	"""
+	Interface / fully abstract parent class for strategies for turning string names into colors
+
+	@note: It's a bit un-pythonic to do this but, given long term extendability concerns, this option was taken
+	"""
+
+	def get_color(self, name):
+		"""
+		Resolves this name of a color to an actual color
+
+		@param name: The name of the color desired
+		@type name: String
+		@return: Color corresponding to the given name
+		@rtype: VirtualObjectColor
+		"""
+
+		raise NotImplementedError("Must use subclass / implementor of this interface")
+
+class VirtualObjectSize:
+	"""
+	Structure representing an object's size that implements both the list and iterator "interfaces"
+	"""
+
+	def __init__(self, dimensions):
+		"""
+		Constructor for VirtualObjectSize
+
+		@param dimensions: Object specific dimensions
+		@type dimensions: list of floats
+		"""
+
+		self.__dimensions = dimensions
+	
+	def iter(self):
+		""" Returns iterator over the dimensions of this VirtualObjectSize
+
+		@return: Dimensions of this size
+		@rtype: iterator
+		"""
+		return self.__dimensions.iter()
+	
+	def __iter__(self):
+		""" Returns iterator over the dimensions of this VirtualObjectSize
+
+		@return: Dimensions of this size
+		@rtype: iterator
+		"""
+		return self.__dimensions.iter
+	
+	def __setitem__(self, key, value):
+		"""
+		Sets the given dimension to the provided value 
+
+		@param key: Index in this set of dimensions
+		@type key: integer
+		@param value: The new value of this dimension
+		@type value: float
+		"""
+		self.__dimensions[key] = value
+	
+	def __getitem__(self, key):
+		"""
+		Sets the given dimension to the provided value 
+
+		@param key: Index in this set of dimensions
+		@type key: integer
+		@return: Value
+		@rtype: float
+		"""
+		return self.__dimensions[key]
+	
+	def __delitem__(self, key):
+		"""
+		Removes the given dimension from this size
+
+		@param key: Index in this set of dimensions
+		@type key: integer
+		"""
+		del self.__dimensions[key]
+
+class NamedSizeResolver:
+	"""
+	Interface / fully abstract parent class for strategies for turning names into sizes
+	"""
+
+	def get_size(self, name):
+		"""
+		Resolves this name of a size to an actual size
+
+		@param name: The name of the size desired
+		@type name: String
+		@return: Size corresponding to the given name
+		@rtype: VirtualObjectSize
+		"""
+
+		raise NotImplementedError("Must use a subclass / implementor of this interface")
+
+class VirtualObjectBuilder:
+	""" 
+	Creates virtual objects for a given inverse kinematics package
+
+	@note: This should be created and accessed through an ObjectManipulationFacade
+	"""
+
+	NOT_SPECIFIED = None
+
+	def __init__(self, construction_strategy, named_size_resolver, color_resolution_strategy):
+		"""
+		Creates a new VirtualObjectBuilder that leverages the given construction strategy
+		
+		@param construction_strategy: Package specific strategy for building virtual objects specific to the inverse kinematics software in use
+		@type: Subclass of VirtualObjectConstructionStrategy
+		@param named_size_resolver: Strategy for turning names into sizes for name - descriptor pairs
+		@type named_size_resolver: Subclass of NamedSizeResolver
+		@param color_resolution_strategy: A strategy for turning strings passed in for colors to VirtualObjectColor objects
+		@type color_resolution_strategy: Subclass of ColorResolutionStrategy
+		"""
+
+		self.__construction_strategy = construction_strategy
+		self.__named_size_resolver = named_size_resolver
+		self.__color_resolution_strategy = color_resolution_strategy
+		self.__descriptor = VirtualObjectBuilder.NOT_SPECIFIED
+		self.__color = VirtualObjectBuilder.NOT_SPECIFIED
+		self.__size = VirtualObjectBuilder.NOT_SPECIFIED
+		self.__name = VirtualObjectBuilder.NOT_SPECIFIED
+	
+	def set_name(self, new_name):
+		"""
+		Sets the name of the next object and following objects to be created
+
+		@param new_name: The name to give to the next object and following objects to be created
+		@type new_name: String
+		"""
+
+		self.__name = new_name
+	
+	def set_descriptor(self, new_descriptor):
+		"""
+		Sets the descriptor of the next object and following objects to be created
+
+		@param new_descriptor: The descriptor to give to the next object and following objects to be created
+		@type new_descriptor: String
+		"""
+
+		self.__descriptor = new_descriptor
+	
+	def set_color(self, new_color):
+		"""
+		Sets the color of the next object and following objects to be created
+
+		@param new_color: The color to give to the next object and following objects to be created
+		@type new_color: String
+		"""
+
+		self.__color = new_color
+	
+	def set_size(self, new_size):
+		"""
+		Sets the size of the next object and following objects to be created
+
+		@param new_size: The size to give to the next object and following objects to be created
+		@type new_size: VirtualObjectSize
+		"""
+
+		self.__size = new_size
+	
+	def create(self, position):
+		"""
+		Creates a new object at the given position
+
+		@param position: The position to give this new object
+		@type position: ObjectPosition
+		@return: A new virtual object
+		@rtype: VirtualObject
+
+		@note: This does not add this object to the simulation. Make sure not to call this directly. Use ObjectManipulationFacade instead
+		"""
+
+		if self.__descriptor == VirtualObjectBuilder.NOT_SPECIFIED:
+			raise AttributeError("Descriptor has not been set. Please call set_descriptor and try again.")
+
+		if self.__color == VirtualObjectBuilder.NOT_SPECIFIED:
+			raise AttributeError("Color has not been set. Please call set_color and try again.")
+
+		if self.__size == VirtualObjectBuilder.NOT_SPECIFIED:
+			raise AttributeError("Size has not been set. Please call set_size and try again.")
+
+		if self.__name == VirtualObjectBuilder.NOT_SPECIFIED:
+			raise AttributeError("Name has not been set. Please call set_name and try again.")
+		
+		# resolve color
+		color = self.__color_resolution_strategy(self.__color)
+
+		# resolve size
+		size = self.__named_size_resolver(self.__size)
+
+		# Create and return new object
+		return self.__construction_strategy.create_object(descriptor, color, size)
+		
---
+++ state.py
@@ -0,0 +1,252 @@
+"""
+Module containing objects providing management of state
+
+This module provides state containing objects that manage state inherent to inverse kinematics simulations
+
+@author: Sam Pottinger
+@license: GNU General Public License v2
+@copyright: 2011
+@organization: Andrews Robotics Initiative at CU Boulder
+"""
+
+class ObjectPosition:
+	"""
+	Immutable generic position / orientation flyweight in an inverse kinematics simulation
+
+	Package independent representation of a position and orientation in the target inverse kinematics software
+
+	@note: Should be created through an ObjectPositionFactory
+	"""
+
+	def __init__(x, y, z, roll, pitch, yaw):
+		""" Default constructor for an ObjectPosition
+
+		@param x: The x component of this position
+		@type x: float
+		@param y: The y component of this position
+		@type y: float
+		@param z: The z component of this position
+		@type z: float
+		@param roll: The roll (rotation about x axis) component of this orientation
+		@type roll: float
+		@param pitch: The pitch (rotation about y axis) component of this orientation
+		@type pitch: float
+		@param yaw: The yaw (rotation about the z axis) component of this orientation
+		@type yaw: float """
+
+		self.__x = x
+		self.__y = y
+		self.__z = z
+		self.__roll = roll
+		self.__pitch = pitch
+		self.__yaw = yaw
+	
+	def get_x():
+		""" Determine's this position's x component
+
+		@return: This position's x component
+		@rtype: float """
+
+		return self.__x
+	
+	def get_y():
+		""" Determine's this position's y component
+
+		@return: This position's y component
+		@rtype: float """
+
+		return self.__y
+	
+	def get_z():
+		""" Determine's this position's z component
+
+		@return: This position's z component
+		@rtype: float """
+
+		return self.__z
+	
+	def get_roll():
+		""" Determine's this orientations's x component (roll)
+
+		@return: This orientation's roll
+		@rtype: float """
+
+		return self.__roll
+	
+	def get_pitch():
+		""" Determine's this orientations's y component (pitch)
+
+		@return: This orientation's pitch
+		@rtype: float """
+
+		return self.__pitch
+	
+	def get_yaw():
+		""" Determine's this orientations's z component (yaw)
+
+		@return: This orientation's yaw
+		@rtype: float """
+
+		return self.__yaw
+
+
+
+class ObjectPositionFactory:
+	"""
+	Factory to create ObjectPositions that leverages predefined names, defaults, and other conveniences.
+
+	@note: Should be created through an ObjectManipulationFacade and its ObjectPositionFactoryConstructor
+	"""
+
+	DEFAULT = None
+	
+	def __init__(self, default_roll, default_pitch, default_yaw, prefabricated_positions):
+		""" Constructor for an ObjectPositionFactory
+		
+		@param default_roll: The default roll applied to new positions
+		@type default_roll: float
+		@param default_pitch: The default pitch applied to new positions
+		@type default_pitch: float
+		@param default_yaw: The default yaw applied to new positions
+		@type default_yaw: float
+		@param prefabricated_positions: Named default positions
+		@type prefabricated_positions: Dictionary of String to ObjectPosition"""
+
+		self.__default_roll = default_roll
+		self.__default_pitch = default_pitch
+		self.__default_yaw = default_yaw
+		self.__prefabricated_positions = prefabricated_positions
+	
+	def create_position(self, x, y, z, roll=ObjectPositionFactory.DEFAULT, pitch=ObjectPositionFactory.DEFAULT, yaw=ObjectPositionFactory.DEFAULT):
+
+		""" Creates a new position by values
+
+		@param x: The x component of this new position
+		@type x: float
+		@param y: The y component of this new position
+		@type y: float
+		@param z: The z component of this new position
+		@type z: float
+		@keyword roll: The roll component of this new orientation, defaults to the roll provided in init
+		@type: roll: float
+		@keyword pitch: The pitch component of this new orientation, defaults to the pitch provided in init
+		@type: pitch: float
+		@keyword yaw: The yaw component of this new orientation, defaults to the yaw provided in init
+		@type: yaw: float 
+		@return: New position that fits the provided parameters
+		@type: ObjectPosition"""
+
+		# Resolve defaults
+		if roll == ObjectPosition.DEFAULT:
+			roll = self.__default_roll
+
+		if pitch == ObjectPosition.DEFAULT:
+			pitch = self.__default_pitch
+
+		if yaw == ObjectPosition.DEFAULT:
+			yaw = self.__default_yaw
+		
+		return ObjectPosition(x, y, z, roll, pitch, yaw)
+	
+	def create_prefabricated(name, x=ObjectPositionFactory.DEFAULT, y=ObjectPositionFactory.DEFAULT, z=ObjectPositionFactory.DEFAULT, roll=ObjectPositionFactory.DEFAULT, pitch=ObjectPositionFactory.DEFAULT, yaw=ObjectPositionFactory.DEFAULT):
+		""" Creates a new position based off of the named prefabricated position
+			
+		@param name: The name of the prefabrication to base this new position off of
+		@type name: String
+		@keyword x: Specifies the x component of the new position, defaults to the x position specified in the desired prefabrication
+		@type x: float
+		@keyword y: Specifies the y component of the new position, defaults to the y position specified in the desired prefabrication
+		@type y: float
+		@keyword z: Specifies the z component of the new position, defaults to the z position specified in the desired prefabrication
+		@type z: float
+		@keyword roll: Specifies the roll of the new orientation, defaults to the roll specified in the desired prefabrication
+		@type roll: float
+		@keyword pitch: Specifies the pitch of the new orientation, defaults to the pitch specified in the desired prefabrication
+		@type pitch: float
+		@keyword yaw: Specifies the yaw of the new orientation, defaults to the yaw specified in the desired prefabrication
+		@type yaw: float 
+		@raise ValueError: Raised if the requested prefabricated position has not been specified previously"""
+	
+		# Attempt to find the prefabrication, throwing an exception if not available
+		if not name in self.__prefabricated_positions:
+			raise ValueError("Prefabricated position not defined")
+		
+		position = self.__prefabricated_positions[name]
+
+		return self.clone(position, x, y, z, roll, pitch, yaw)
+		
+	def clone(position, x=ObjectPositionFactory.DEFAULT, y=ObjectPositionFactory.DEFAULT, z=ObjectPositionFactory.DEFAULT, roll=ObjectPositionFactory.DEFAULT, pitch=ObjectPositionFactory.DEFAULT, yaw=ObjectPositionFactory.DEFAULT):
+		""" Creates a new position based off of the given position 
+
+		@param position: The position to base the new position off of
+		@type: ObjectPosition
+		@keyword x: Specifies the x component of the new position, defaults to the x position specified in the position provided to clone
+		@type x: float
+		@keyword y: Specifies the y component of the new position, defaults to the y position specified in the position provided to clone
+		@type y: float
+		@keyword z: Specifies the z component of the new position, defaults to the z position specified in the position provided to clone
+		@type z: float
+		@keyword roll: Specifies the roll of the new orientation, defaults to the roll specified in the position provided to clone
+		@type roll: float
+		@keyword pitch: Specifies the pitch of the new orientation, defaults to the pitch specified in the position provided to clone
+		@type pitch: float
+		@keyword yaw: Specifies the yaw of the new orientation, defaults to the yaw specified in the position provided to clone
+		@type yaw: float """
+
+		if x == ObjectPositionFactory.DEFAULT:
+			x = position.get_x()
+		
+		if y == ObjectPositionFactory.DEFAULT:
+			y = position.get_y()
+		
+		if z == ObjectPositionFactory.DEFAULT:
+			z = position.get_z()
+
+		if roll == ObjectPositionFactory.DEFAULT:
+			roll = position.get_roll()
+		
+		if pitch == ObjectPositionFactory.DEFAULT:
+			pitch = position.get_pitch()
+		
+		if yaw == ObjectPositionFactory.DEFAULT:
+			yaw = position.get_yaw()
+		
+		return ObjectPosition(x, y, z, roll, pitch, yaw)
+	
+	def create_position_relative(position, x, y, z, roll=ObjectPositionFactory.DEFAULT, pitch=ObjectPositionFactory.DEFAULT, yaw=ObjectPositionFactory.DEFAULT):
+		""" Creates a new position based off of the given position 
+
+		@param position: The position to base the new position off of
+		@type: ObjectPosition
+		@param x: Specifies the x component of the new position, computed relative to the provided existing position
+		@type x: float
+		@param y: Specifies the y component of the new position, computed relative to the provided existing position
+		@type y: float
+		@param z: Specifies the z component of the new position, computed relative to the provided existing position
+		@type z: float
+		@keyword roll: Specifies the roll of the new orientation, defaults to the roll specified when this factory was created
+		@type roll: float
+		@keyword pitch: Specifies the pitch of the new orientation, defaults to the pitch specified when this factory was created
+		@type pitch: float
+		@keyword yaw: Specifies the yaw of the new orientation, defaults to the yaw specified when this factory was created
+		@type yaw: float """
+
+		# Resolve defaults
+		if roll == ObjectPosition.DEFAULT:
+			roll = self.__default_roll
+
+		if pitch == ObjectPosition.DEFAULT:
+			pitch = self.__default_pitch
+
+		if yaw == ObjectPosition.DEFAULT:
+			yaw = self.__default_yaw
+		
+		# Compute relative values
+		x = x + position.get_x()
+		y = y + position.get_y()
+		z = z + position.get_z()
+		roll = roll + position.get_roll()
+		pitch = pitch + position.get_pitch()
+		yaw = yaw + position.get_yaw()
+
+		return ObjectPosition(x, y, z, roll, pitch, yaw)
